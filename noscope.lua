2521ae530060b13488656e50803f0c3f0b3e0c62
Orchides <96978561+Orchides@users.noreply.github.com>
2022-02-19 16:10:54 +0100

    Create noscope.lua

---
+++ noscope.lua
@@ -0,0 +1,13 @@
+getgenv().Settings = {
+
+   Fov = 1000,
+
+   Hitbox = "Head",
+
+   FovCircle = false,
+
+}
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WetCheezit/Releases/main/No-Scope-Arcade/Silent%20aim.lua"))()
