f4c6b2b930f43f40e9cb13c56e5bbc45d58690af
Orchides <96978561+Orchides@users.noreply.github.com>
2022-02-25 10:44:39 +0100

    Update NinjaSimulator.lua

--- NinjaSimulator.lua
+++ NinjaSimulator.lua
@@ -1,174 +1,38 @@
 local Config = {
-
-    WindowName = "OP NINJA SIMULATOR",
-
-
-
+WindowName = "OP NINJA SIMULATOR",
 Color = Color3.fromRGB(30,144,255),
-
-
-
 Keybind = Enum.KeyCode.RightShift 
-
-
-
 }
-
-
-
-
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Roblox/main/BracketV3.lua"))()
-
-
-
 local Window = Library:CreateWindow(Config, game:GetService("CoreGui"))
-
-
-
-
-
-
-
 local Tab1 = Window:CreateTab("Sub To C02 Script")
-
-
-
-
-
-
-
-local Section1 = Tab1:CreateSection("Auto Farming Section")
-
-
-
-
-
-
-
+local Section1 = Tab1:CreateSection("Auto Farm")
 function tool()
-
-
-
     for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-
-
-
         if v.Name ~= "Shuriken"  then
-
             if v.Name ~= "TeleportTool" then
-
-
-
           local tools = v
-
-
-
           return tools
-
-
-
             end
-
         end
-
     end
-
 end
-
-
-
-
-
-
-
-
-
-
-
 local autofarm = Section1:CreateToggle("Auto Farm", nil, function(State)
-
-
-
 local character = game.Players.LocalPlayer.Character
-
-
-
-
-
-
-
 for i,v in pairs(character:GetChildren()) do
-
-
-
 if v:IsA("Tool") then
-
-
-
-    equipped = true
-
-
-
-    else
-
-
-
-  equipped = false
-
-
-
-    end
-
+equipped = true
+else
+equipped = false
 end
-
-    local tool = tool()
-
-
-
-    if equipped ~= true then
-
-
-
+end
+local tool = tool()
+if equipped ~= true then
 game.Players.LocalPlayer.Character.Humanoid:EquipTool(tool)
-
-
-
 end
-
 s = State
-
 while s == true do wait()
-
 local vu = game:GetService("VirtualUser")
-
 vu:Button1Down(Vector2.new())
-
-                end
-
-        end)
-
-
-
-
-
-
-
-
-
-
-
-local autoequip = Section1:CreateToggle("Auto Katana", nil, function(State)
-
-  tog = State
-
-  while tog do wait()
-
-        local tool = tool()
-
-        game.Players.LocalPlayer.Character.Humanoid:EquipTool(tool)
-
-
-
-      end
+end
 end)
